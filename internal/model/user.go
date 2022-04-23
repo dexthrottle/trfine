8c91028b724dad6aa03a17b742c0a5bcf0f2197e
todd <todd.develop@gmail.com>
2022-04-23 22:23:43 +0300

    upd

--- internal/model/user.go
+++ internal/model/user.go
@@ -1,11 +1,9 @@
 package model
 
 type User struct {
-	ID         uint64      `gorm:"primary_key:auto_increment" json:"id"`
-	FirstName  string      `gorm:"type:varchar(255)" json:"first_name"`
-	LastName   string      `gorm:"type:varchar(255)" json:"last_name"`
-	Username   string      `gorm:"type:varchar(255)" json:"username"`
-	UserTGId   int         `gorm:"type:int;unique;not null" json:"user_tg_id"`
-	Posts      *[]Post     `json:"posts,omitempty"`
-	Categories *[]Category `json:"categories,omitempty"`
+	ID        uint64 `gorm:"primary_key:auto_increment" json:"id"`
+	FirstName string `gorm:"type:varchar(255)" json:"first_name"`
+	LastName  string `gorm:"type:varchar(255)" json:"last_name"`
+	Username  string `gorm:"type:varchar(255)" json:"username"`
+	UserTGId  int    `gorm:"type:int;unique;not null" json:"user_tg_id"`
 }
---
+++ internal/service/app_config.go
@@ -0,0 +1,43 @@
+package service
+
+import (
+	"context"
+
+	"github.com/dexthrottle/trfine/internal/dto"
+	"github.com/dexthrottle/trfine/internal/model"
+	"github.com/dexthrottle/trfine/internal/repository"
+	"github.com/mashingan/smapping"
+)
+
+type AppConfigService interface {
+	InsertAppConfig(ctx context.Context, appCfg dto.AppConfigDTO) (*model.AppConfig, error)
+}
+
+type appCfgService struct {
+	ctx              context.Context
+	appCfgRepository repository.AppConfigRepository
+}
+
+func NewAppCfgRepository(
+	ctx context.Context,
+	appCfgRepository repository.AppConfigRepository,
+) AppConfigService {
+	return &appCfgService{
+		ctx:              ctx,
+		appCfgRepository: appCfgRepository,
+	}
+}
+
+func (s *appCfgService) InsertAppConfig(ctx context.Context, appCfgDTO dto.AppConfigDTO) (*model.AppConfig, error) {
+	appCfgToCreate := model.AppConfig{}
+	err := smapping.FillStruct(&appCfgToCreate, smapping.MapFields(&appCfgDTO))
+	if err != nil {
+		return nil, err
+	}
+
+	updatedAppCfg, err := s.appCfgRepository.InsertAppConfig(ctx, appCfgToCreate)
+	if err != nil {
+		return nil, err
+	}
+	return updatedAppCfg, nil
+}
