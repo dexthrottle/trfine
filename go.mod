b3dc977145f8295b1eaf434bf4c3d97294b0ddde
crot <todd.develop@gmail.com>
2022-04-27 01:01:28 -0400

    remove gin and server

--- go.mod
+++ go.mod
@@ -8,25 +8,11 @@ require (
 )
 
 require (
-	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/glebarez/go-sqlite v1.16.0 // indirect
-	github.com/go-playground/locales v0.13.0 // indirect
-	github.com/go-playground/universal-translator v0.17.0 // indirect
-	github.com/go-playground/validator/v10 v10.4.1 // indirect
-	github.com/golang/protobuf v1.3.3 // indirect
 	github.com/google/uuid v1.3.0 // indirect
-	github.com/json-iterator/go v1.1.9 // indirect
-	github.com/kr/pretty v0.1.0 // indirect
-	github.com/leodido/go-urn v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
-	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
-	github.com/modern-go/reflect2 v0.0.0-20180701023420-4b7aa43c6742 // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20200410134404-eec4a21b6bb0 // indirect
-	github.com/ugorji/go/codec v1.1.7 // indirect
-	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
 	golang.org/x/sys v0.0.0-20220405052023-b1e9470b6e64 // indirect
-	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
-	gopkg.in/yaml.v2 v2.2.8 // indirect
 	modernc.org/libc v1.14.12 // indirect
 	modernc.org/mathutil v1.4.1 // indirect
 	modernc.org/memory v1.0.7 // indirect
@@ -34,7 +20,6 @@ require (
 )
 
 require (
-	github.com/gin-gonic/gin v1.7.7
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.4 // indirect
 	github.com/mashingan/smapping v0.1.13
--- internal/handler/user.go
+++ internal/handler/user.go
@@ -1,58 +1,13 @@
 package handler
 
 import (
-	"net/http"
-
-	"github.com/dexthrottle/trfine/internal/dto"
-	"github.com/dexthrottle/trfine/internal/helper"
-	"github.com/gin-gonic/gin"
+	"context"
 )
 
-func (c *Handler) CreateUser(ctx *gin.Context) {
-	var createDTO dto.CreateUserDTO
-	errDTO := ctx.ShouldBind(&createDTO)
-	if errDTO != nil {
-		response := helper.BuildErrorResponse("Failed to process request", errDTO.Error(), helper.EmptyObj{})
-		ctx.AbortWithStatusJSON(http.StatusBadRequest, response)
-		return
-	}
+func (c *Handler) CreateUser(ctx context.Context) {
 
-	userStatus, err := c.service.User.IsDuplicateUserTGID(ctx, createDTO.UserTGId)
-	c.log.Errorln(err)
-	if err == nil || userStatus {
-		response := helper.BuildErrorResponse("Failed to process request", "Duplicate user_tg_id", helper.EmptyObj{})
-		ctx.JSON(http.StatusConflict, response)
-		return
-	} else {
-		createdUser, err := c.service.User.Insert(ctx, createDTO)
-		if err != nil {
-			c.log.Errorf("create user failed: %v", err)
-			response := helper.BuildErrorResponse("Failed to process request", err.Error(), helper.EmptyObj{})
-			ctx.JSON(http.StatusConflict, response)
-			return
-		}
-		response := helper.BuildResponse(true, "OK!", createdUser)
-		ctx.JSON(http.StatusCreated, response)
-	}
 }
 
-func (c *Handler) ProfileUser(ctx *gin.Context) {
-
-	id := ctx.GetHeader("user_tg_id")
-	user, err := c.service.User.Profile(ctx, id)
-	if err != nil {
-		c.log.Errorf("profile user error: %v", err)
-		response := helper.BuildErrorResponse("Failed to process request", err.Error(), helper.EmptyObj{})
-		ctx.JSON(http.StatusConflict, response)
-		return
-	}
-
-	if user.UserTGId == 0 {
-		response := helper.BuildErrorResponse("Failed to process request", "Такого пользователя нет", helper.EmptyObj{})
-		ctx.JSON(http.StatusNotFound, response)
-		return
-	}
-	res := helper.BuildResponse(true, "OK", user)
-	ctx.JSON(http.StatusOK, res)
+func (c *Handler) ProfileUser(ctx context.Context) {
 
 }
